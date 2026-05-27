8941106f3adc6242832e55d49a46d3481654edc1
NayPaingSoe <naypaingsoe042@gmail.com>
2026-05-27 17:51:47 +0630

    fix:ui

--- src/app/mahjong/MahjongPixiTable.tsx
+++ src/app/mahjong/MahjongPixiTable.tsx
@@ -1188,7 +1188,7 @@ export default function MahjongPixiTable({
               const sideCountLeft = counts.left ?? 7;
               const sideCountRight = counts.right ?? 7;
               const sideCount = Math.max(sideCountLeft, sideCountRight);
-              const sideXLeft = tableX + 116;
+              const sideXLeft = tableX + 130;
               const sideXRight = tableX + tableW - 18 - smallH - 116;
               const sideStartY = Math.floor(
                 tableY +
